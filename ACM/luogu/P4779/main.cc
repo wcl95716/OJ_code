3b20aaf8ab35c3e5a651c891503cae9e3e13fac5
panda <347038217@qq.com>
2024-04-09 17:29:01 +0800

    update

--- ACM/luogu/P4779/main.cc
+++ ACM/luogu/P4779/main.cc
@@ -2,16 +2,19 @@
 #include <vector>
 #include <set>
 #include <queue>
+#include <cstring>
 using namespace std;
 const int maxn = 1000*1000;
 
 struct edge {
   int v, w;
+  edge(int v, int w) : v(v), w(w) {}
 };
+
 
 struct node {
   int dis, u;
-
+  node(int dis, int u) : dis(dis), u(u) {}
   bool operator>(const node& a) const { return dis > a.dis; }
 };
 
@@ -22,7 +25,7 @@ priority_queue<node, vector<node>, greater<node> > q;
 void dijkstra(int n, int s) {
   memset(dis, 63, sizeof(dis));
   dis[s] = 0;
-  q.push({0, s});
+  q.push(node(0, s));
   while (!q.empty()) {
     int u = q.top().u;
     q.pop();
@@ -32,13 +35,24 @@ void dijkstra(int n, int s) {
       int v = ed.v, w = ed.w;
       if (dis[v] > dis[u] + w) {
         dis[v] = dis[u] + w;
-        q.push({dis[v], v});
+        q.push(node(dis[v], v));
       }
     }
   }
 }
 
 void run() {
+    int n,m,s;
+    cin>> n >> m >>s;
+    for (int i = 0; i < m; i++){
+        int u,v,w;
+        cin>> u >> v >> w;
+        e[u].push_back(edge(v, w));
+    }
+    dijkstra(n,s);
+    for (int i = 1; i <= n; i++){
+        cout<< dis[i] << " ";
+    }
 
 }
 
